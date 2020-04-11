322bded68a3d8a9d02d4bf50e4aed028822a403b
nmorse <morse.nate@gmail.com>
2020-04-11 12:08:37 -0400

    user input rough sketch

--- src/main.js
+++ src/main.js
@@ -4,4 +4,7 @@ import repl from './pounceEnv.js';
 // logs will still point to your original source modules
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
 
-repl();
+// Add event listener for programmer input
+const el = document.getElementById("user-pl");
+el.addEventListener("keyup", (e) => repl(e.target.value), false);
+repl("0 1 [dup2 +] 5 times");
--- src/pounceEnv.js
+++ src/pounceEnv.js
@@ -1,14 +1,76 @@
 import { purr, pinna } from '@pounce-lang/core';
 
-var stackEle = document.querySelector('#stack');
-var programEle = document.querySelector('#program');
+const stackEle = document.querySelector('#stack');
+const programEle = document.querySelector('#program');
+let interp;
 
-const interp = purr(pinna("2 dup + dup + dup + dup + dup + dup +"), undefined, {debug:true});
-export default function repl() {
+export default function repl(pl) {
+    while (stackEle.firstChild) {
+        stackEle.firstChild.remove();
+    }
+    while (programEle.firstChild) {
+        programEle.firstChild.remove();
+    }
+
+    interp = purr(pinna(pl), undefined, { debug: true });
+    repl_aux();
+};
+
+const repl_aux = () => {
     let result = interp.next();
-    stackEle.textContent = result.value[0];
-    programEle.textContent = result.value[1];
+
+    let newStEle = document.createElement('div');
+    newStEle.innerText = result.value[0].length ? unParse(result.value[0]) + " |" : "|";
+    stackEle.appendChild(newStEle);
+
+    let newPrEle = document.createElement('div');
+    newPrEle.innerText = unParse(result.value[1]);
+    programEle.appendChild(newPrEle);
+
     if (result.value[2]) {
-        setTimeout(repl, 1000);
+        setTimeout(repl_aux, 0);
     }
 };
+
+function unParse(pl) {
+    let ps = '';
+    let spacer = '';
+    for (let i in pl) {
+        if (pl[i] && typeof pl[i] == "object") {
+            if (Array.isArray(pl[i])) {
+                ps += spacer + '[' + unParse(pl[i]) + ']';
+            }
+            else {
+                ps += spacer + '{' + unParseKeyValuePair(pl[i]) + '}';
+            }
+        }
+        else {
+            ps += spacer + pl[i];
+        }
+        spacer = ' ';
+    }
+    return ps;
+}
+
+function unParseKeyValuePair(pl) {
+    let ps = '';
+    let spacer = '';
+    for (let i in pl) {
+        if (pl.hasOwnProperty(i)) {
+            if (pl[i] && typeof pl[i] == "object") {
+                if (Array.isArray(pl[i])) {
+                    ps += spacer + i + ':[' + unParse(pl[i]) + ']';
+                }
+                else {
+                    ps += spacer + i + ':{' + unParseKeyValuePair(pl[i]) + '}';
+                }
+            }
+            else {
+                ps += spacer + i + ':' + pl[i];
+            }
+            spacer = ' ';
+        }
+    }
+    return ps;
+}
+
