260caeb5bfe626371add2bf6485a144f261e8000
nmorse <morse.nate@gmail.com>
2020-05-18 22:51:21 -0400

    1.2.1

--- src/main.js
+++ src/main.js
@@ -10,8 +10,9 @@ const myDebugModeCkbxEle = document.getElementById("debug");
 
 // 0 1 [dup2 +] 5 times
 // [dup 1 - dup 0 > [[*] dip fac] [drop drop] ifte] [fac] def 5 [1 swap] apply fac 
-// 5 [1 swap] [dup 1 -] [dup 0 >] [[*] dip] [drop drop] linrec
-let pounceProgram = "5 [1 swap] [dup 1 -] [dup 0 >] [[*] dip] [drop drop] linrec";
+// 5 [1 swap] [dup 1 -] [dup 0 >] [[*] dip] [drop drop] constrec
+// 5 [dup 0 ==] [1 +] [dup 1 -] [*] linrec
+let pounceProgram = "5 [1 swap] [dup 1 -] [dup 0 >] [[*] dip] [drop drop] constrec";
 let debugMode = myDebugModeCkbxEle.checked;
 
 myPounceProgramEle.addEventListener("keyup", (e) => {
